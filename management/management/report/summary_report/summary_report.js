14bf34de7d1646440fb7b4e92d9dbb42c2e28d17
Gopi Kathiriya <gopikathiriya2003@gmail.com>
2023-11-23 16:34:32 +0530

    create summary report

---
+++ management/management/report/summary_report/summary_report.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, frappe and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Summary Report"] = {
+	"filters": [
+		{
+			'fieldname': 'date',
+			'label': 'Date',
+			'fieldtype': 'Date'
+			
+		},
+
+	]
+};
---
+++ management/management/report/summary_report/summary_report.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2023, frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+   
+    return columns,data
+
+def get_columns():
+    columns = [
+    
+		{
+			'fieldname': 'date',
+			'label': 'Date',
+			'fieldtype': 'Date'
+			
+		},
+         {
+			'fieldname': 'total_buy_liters',
+			'label': 'Buy Milk',
+			'fieldtype': 'Float'
+			
+		},
+        {
+			'fieldname': 'total_sale_liters',
+			'label': 'Sale Milk',
+			'fieldtype': 'Float'
+			
+		},
+        
+    ]
+    return columns
+def get_data(filters=None):
+    filter_list = []
+    
+    
+    if filters and filters.get("date"):
+        filter_list.append(["date", "=", filters["date"]])
+
+    data = frappe.get_list("Stock Summary", filters=filter_list, fields=["date","total_buy_liters","total_sale_liters"])
+    return data
