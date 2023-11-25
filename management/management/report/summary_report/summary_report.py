d4ea8460e454a684a20b8f91943eb81e33c1775e
Gopi Kathiriya <gopikathiriya2003@gmail.com>
2023-11-25 11:54:27 +0530

    Add the total into the summary report

--- management/management/report/summary_report/summary_report.py
+++ management/management/report/summary_report/summary_report.py
@@ -39,12 +39,26 @@ def get_columns():
         
     ]
     return columns
+
+
 def get_data(filters=None):
     filter_list = []
     
-    
     if filters and filters.get("date"):
-        filter_list.append(["date", "=", filters["date"]])
+        filter_list.append("date", "=", filters["date"])
 
     data = frappe.get_list("Stock Summary", filters=filter_list, fields=["date","total_buy_liters","total_sale_liters","pending_stock"])
+
+    total_buy_liters = sum(float(row["total_buy_liters"] or 0) for row in data)
+    total_sale_liters = sum(float(row["total_sale_liters"] or 0) for row in data)
+    total_pending_stock = sum(float(row["pending_stock"] or 0) for row in data)
+
+    totals_row = {
+        "date": "Total",
+        "total_buy_liters": total_buy_liters,
+        "total_sale_liters": total_sale_liters,
+        "pending_stock": total_pending_stock
+    }
+    data.append(totals_row)
+
     return data
